3c162fce602f6f1e2c3abe6bb082eb3c75752d72
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-04-21 22:57:57 -0400

    Attempt 1

---
+++ shortest_distance_char.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] shortestToChar(String S, char C) {
+        int[] nums = new int[S.length()];
+        for(int i=0;i<nums.length;i++)
+            nums[i] = Integer.MAX_VALUE;
+        /*Stack<Integer> stack = new Stack<>();
+        
+        boolean nFirst = false;
+        int j =1;*/
+        for(int i=0;i<S.length();i++)
+        {
+            if(S.charAt(i) == C)
+            {
+                nums[i] = 0;
+                int count1 =1, j=i;
+                if(j-->=0)
+                    j--;
+                while(j>=0 && S.charAt(j)!=C)
+                {
+                    nums[j] = (int) (Math.min(nums[j],count1++));
+                    j--;
+                }
+                int k=i, count2=1;
+                if(k++<=nums.length-1)
+                    k++;
+                while(k<=nums.length-1 && S.charAt(k)!=C)
+                {
+                    nums[k] = Math.min(nums[k], count2++);
+                    k++;
+                }
+            }
+                
+            }
+                return nums;
+        }
+}
