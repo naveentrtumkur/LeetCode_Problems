de6becade13243324818f74d2a47ae7259adbb33
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-04-25 23:41:25 -0400

    house robber java problem.
    house robber java probelm.

--- House_robber.java
+++ House_robber.java
@@ -1,3 +1,16 @@
+
+public int rob(int[] num) {
+    int rob = 0; //max monney can get if rob current house
+    int notrob = 0; //max money can get if not rob current house
+    for(int i=0; i<num.length; i++) {
+        int currob = notrob + num[i]; //if rob current value, previous house must not be robbed
+        notrob = Math.max(notrob, rob); //if not rob ith house, take the max value of robbed (i-1)th house and not rob (i-1)th house
+        rob = currob;
+    }
+    return Math.max(rob, notrob);
+}
+
+
 class Solution {
     public int rob(int[] nums) {
         int prevNo =0, prevyes =0;
