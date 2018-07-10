de3f71805d589e44f734c4e8fe8b4bda2170707d
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-07-10 19:30:03 -0400

    Find right interval approach
    Explanation of approach to finding right interval

---
+++ 436_Find_right_interval.java
@@ -0,0 +1,18 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+
+// Solution would be to sort the Intervals based on the start-points and then 
+// choose the statrting intervals closest to the previous ending interval.
+
+class Solution {
+    public int[] findRightInterval(Interval[] intervals) {
+        
+    }
+}
