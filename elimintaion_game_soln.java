7453d689df1421d8bc89b5487d6915fda788ac49
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-07-10 19:21:32 -0400

    ELimintaion game soln
    Soln of elimination game

---
+++ elimintaion_game_soln.java
@@ -0,0 +1,11 @@
+// approach -- Use two stacks.
+// Step-1: push all teh elements in reverse order onto first stack.
+// Step-2: pop first element and discard, pop second element onto second stack.
+// Base case: If input is null or input is one element return that.
+// else return when the number of element is just one.
+
+class Solution {
+    public int lastRemaining(int n) {
+        
+    }
+}
