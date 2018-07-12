c733c92188add79d82128a8f746810cc3e9f88c1
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-07-12 09:31:04 -0400

    Simple check
    Simple check

---
+++ 166_Fraction_to_decimal.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String fractionToDecimal(int numerator, int denominator) {
+        Double val = (double)((double)numerator/(double)denominator);
+        //int val = 3;
+        //String str = (String.valueOf(val));
+        System.out.println(Double.toString(val));
+        return "5";
+    }
+}
