489cfc50a140638225eaf3ab012113ccb53aaa6a
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-07-13 09:14:47 -0400

    DP soln.
    DP soln to  problem..

--- 55_jump_game_DP_Soln.java
+++ 55_jump_game_DP_Soln.java
@@ -3,17 +3,17 @@
 // Space O(N)
 enum Index {
     GOOD, BAD, UNKNOWN
-}
+} //enum to store the good, bad andf unknown vals.
 
 public class Solution {
-    Index[] memo;
+    Index[] memo; //array ofindex
 
     public boolean canJumpFromPosition(int position, int[] nums) {
         if (memo[position] != Index.UNKNOWN) {
-            return memo[position] == Index.GOOD ? true : false;
+            return memo[position] == Index.GOOD ? true : false; //if good or bad, return that appropriately..
         }
 
-        int furthestJump = Math.min(position + nums[position], nums.length - 1);
+        int furthestJump = Math.min(position + nums[position], nums.length - 1); //find the furthest jump...
         for (int nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
             if (canJumpFromPosition(nextPosition, nums)) {
                 memo[position] = Index.GOOD;
@@ -21,11 +21,11 @@ public class Solution {
             }
         }
 
-        memo[position] = Index.BAD;
+        memo[position] = Index.BAD; //if no good jump, assign bad and return
         return false;
     }
 
-    public boolean canJump(int[] nums) {
+    public boolean canJump(int[] nums) { //check if a jump can be made & return accordingly.
         memo = new Index[nums.length];
         for (int i = 0; i < memo.length; i++) {
             memo[i] = Index.UNKNOWN;
