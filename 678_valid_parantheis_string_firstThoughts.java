3ed70c01312b5a320d0fc7f9a8e66208f89c6185
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-07-13 17:41:13 -0400

    first thoughts
    first thoughts

---
+++ 678_valid_parantheis_string_firstThoughts.java
@@ -0,0 +1,12 @@
+// My approach.
+// if the left matches right simply incerement, don't push onto stack.
+// when you know all charcs are seen and stack is non-empty, pop char by char.
+// If there is no match then return false.
+
+// If all chars can be matched properly and stack is empty, return true
+
+class Solution {
+    public boolean checkValidString(String s) {
+        
+    }
+}
