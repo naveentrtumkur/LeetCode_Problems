f7302ebe6d25df8632e82c330a22c65d8bfd410d
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-07-15 16:05:29 -0400

    Implemennt Trie in Java
    Implement Trie

---
+++ 208_Implement_Trie_prefixTree.java
@@ -0,0 +1,16 @@
+//Trie or Prefix tree is a very important data structure used in the current world.
+/*
+  Why cant we use HashMaps for tring searches?
+ - Cannot retrieve all prefixes.
+ - If multiple strings are being stored, space restriction and it may seem like we are performing an O(n) search.
+
+// Efficient search, prefix_match can be done using Trie.
+Applicns:-
+1. Google Search
+2. Boggle game (word puzzle)
+3. word document, autocorrection
+4. Ip address routing (Longest prefix match)
+
+**/
+
+// We can efficiently implemnet a Trie in Java 
