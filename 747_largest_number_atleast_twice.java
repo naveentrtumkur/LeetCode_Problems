c8ff7b8c8d8ed830413123dec952e7ffd6ad5ce2
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-07-18 22:39:19 -0400

    Largest number
    largest numb atleast twice

---
+++ 747_largest_number_atleast_twice.java
@@ -0,0 +1,12 @@
+// My approach would be to solve in O(2n) ~ O(n) time.
+
+// 1. In first pass scan to find the maximum element.
+// 2. Do a second pass such that:
+//  - If there's any value > 2*max return -1.
+//  - Else you can return the max_element_index
+
+class Solution {
+    public int dominantIndex(int[] nums) {
+        
+    }
+}
