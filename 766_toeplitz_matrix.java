453166f8095917f32b5885779866a58b15daa0dd
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-07-20 20:52:25 -0400

    toeplitz matrix
    Find whether a matrix is toeplitz

---
+++ 766_toeplitz_matrix.java
@@ -0,0 +1,17 @@
+/***
+
+A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same element.
+
+Now given an M x N matrix, return True if and only if the matrix is Toeplitz.
+
+Ex: 
+
+matrix = [
+  [1,2,3,4],
+  [5,1,2,3],
+  [9,5,1,2]
+]
+
+***/
+
+
