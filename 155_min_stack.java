56c1fa94c48450c743cdeff70a3ccc2bbba9d2ff
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-07-21 19:23:16 -0400

    Minimum stack question
    Implementation using just one stack..

---
+++ 155_min_stack.java
@@ -0,0 +1,40 @@
+// classic stack implementation where we would perform the following operations:-
+// Push(), pop(), peek() and getMin() functionalities implemented.
+
+class MinStack {
+    int min = Integer.MAX_VALUE; //Initialize min value to max initially.
+    Stack<Integer> stack = new Stack<Integer>(); //Instantiate a stack.
+    
+    public void push(int x) {
+        // only push the old minimum value when the current 
+        // minimum value changes after pushing the new value x
+        if(x <= min){          
+            stack.push(min); //push min and update the min value...
+            min=x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        // if pop operation could result in the changing of the current minimum value, 
+        // pop twice and change the current minimum value to the last popped/minimum value.
+        if(stack.pop() == min) min=stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
