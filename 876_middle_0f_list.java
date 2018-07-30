c49b035da0dc44cc82c43f9e8b056807f768ee67
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-07-29 20:34:10 -0400

    approach explianed
    approach

---
+++ 876_middle_0f_list.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+// This is a classic linkedlist problem to find the middle node.
+// Use runner technique with a fast and slow pointer.
+// For every move of slow, move the fast two moves.
+// when fast reaches End of list, slow would be at the middle.
+// Time Complexity: O(n) Space : O(1)
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        
+    }
+}
