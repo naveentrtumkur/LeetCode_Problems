d71da2402db55cb2953a836f4be33ec67d6b891d
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-07-29 22:08:50 -0400

    Stone Game Soln.
    Stone Game SOln.

---
+++ 877_stone_game_soln1.java
@@ -0,0 +1,15 @@
+// There's a trick to solve this problem in 1 line.
+// Just take a small 4 stone example.
+// clearly alex can take either first + third or second + fourth
+// This means Alex can win by either of the two approaches.
+// The output would be Alex will win always.
+
+
+// It becomes trickier if the number of inputs is odd.
+// We need to come up with a DP approach.
+
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
