ddd6c72e4c5170fa3952b06d1020e6750eaa1666
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-07-30 09:43:42 -0400

    Approach explianed.
    Most common word

---
+++ 819_most_common_word.java
@@ -0,0 +1,9 @@
+// Solution: Use a HashMap to store the key and count(frequency) of occurance.
+// Check the highest frequency word, if it's banned go to next word.
+// Else return that as the highest frequency word.
+
+class Solution {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        
+    }
+}
