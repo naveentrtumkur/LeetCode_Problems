cf376a92bb8c5339116366afad8c88d5c381b4cc
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-08-01 16:17:29 -0400

    Roman to integer
    Few thoughts and ideas to improve

---
+++ 13_Roman_to_integer.java
@@ -0,0 +1,10 @@
+// Straight forward solution which IO can think of is use a switch to find the character identified.
+// Store them by using O(n) space.
+// Then in second pass compute the sum and return the value.
+// Question: How to make it efficient by avoiding space and computing in one pass..???
+
+class Solution {
+    public int romanToInt(String s) {
+        
+    }
+}
