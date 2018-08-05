b353243eec41d52f81bacc188a09044b1926d18b
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-08-05 19:02:10 -0400

    thoughts on DP
    Thoiughts to approach this probelm.

---
+++ 646_maximum_length_pair_DP.java
@@ -0,0 +1,11 @@
+// DP Solution is also possible.
+// sort the Array based on first position.
+
+// If max length is k and for any next pair of elements i=[][] and j=[][]
+// check if i[*][x] < j[y][*]. if that's true increment the chain else move forward by assigning previous chain,
+
+class Solution {
+    public int findLongestChain(int[][] pairs) {
+        
+    }
+}
