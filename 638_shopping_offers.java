87288500421be237d778946dc23bd63eb4f09b7a
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-08-05 23:14:59 -0400

    Shopping offers
    Undersatanding the problem.

---
+++ 638_shopping_offers.java
@@ -0,0 +1,10 @@
+// read twice to understand the problem thoroughly.
+// First array represents the cost of each item.
+// Second array represents the special offer and Qty we get.
+// Third array represents the quantity we need to buy to avail the special offer/complete the offer
+
+class Solution {
+    public int shoppingOffers(List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
+        
+    }
+}
