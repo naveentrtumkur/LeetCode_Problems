39681aa1b5dda448c1d3bed7c091398e7a1c2487
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-08-05 21:16:03 -0400

    Bottom up approach
    Soln.

--- 516_Longest_palindrome_sequence.java
+++ 516_Longest_palindrome_sequence.java
@@ -12,5 +12,24 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
         
+        //Declare a 2D Dp array.
+        int[][] dp = new int[s.length()][s.length()];
+        
+        // Use the recurrence to solve the problem.
+        for(int i=s.length()-1; i>=0;i--)
+        {
+            dp[i][i] = 1; //Initialize diagional/same character lengths to 1.
+            for(int j=i+1; j < s.length();j++) //Lopp from j=i+1 to len(s)
+            {
+                //if the characters are the same then increment dp[i][j] by prev +2
+                if(s.charAt(i) == s.charAt(j))
+                    dp[i][j] = dp[i+1][j-1] + 2;
+                else
+                    dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]); //Recurse to get the max length by omitting char @i or char @j
+            }
+        }
+        
+        //return the max length subsequence
+        return dp[0][s.length()-1];
     }
 }
