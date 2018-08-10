4757c6cd59185069275abcced1fd14e48705ab61
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-08-10 08:54:38 -0400

    Thinking process
    Thought process

---
+++ 76_minimum_window_substring.java
@@ -0,0 +1,14 @@
+// Approach/Soln-1: Sliding window approach Using a HashSet (1-pass) O(n) Solution.
+/*  - Start scanning from left to right pushing every encountered character onto HashSet.
+    - Check if all characters are present. If yes note the length and reset hashSet.
+    - Find the minimum window possible and return the result.
+    
+    Time: O(n2) and Space: O(n)
+    */
+
+
+class Solution {
+    public String minWindow(String s, String t) {
+        
+    }
+}
